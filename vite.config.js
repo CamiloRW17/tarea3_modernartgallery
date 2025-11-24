6b527dcbd9684a1c8110f42cf36ee7e6e55d5044
developanther17 <developanther17@gmail.com>
2025-11-24 14:12:29 -0600

    test9

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+
+  base: '/tarea3_modernartgallery/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        location: resolve(__dirname, 'location.html'), 
+      },
+    },
+  },
 })
